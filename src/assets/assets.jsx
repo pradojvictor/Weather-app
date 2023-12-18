1baf3e8ac0ea2f0a8458ab6b05762fe308fc56a7
pradojvictor <joaovictorred@hotmail.com>
2023-12-18 06:20:22 -0300

    style: add icons

---
+++ src/assets/assets.jsx
@@ -0,0 +1,9 @@
+import humidity from './icons/humidity_icon.png';
+import thermal from './icons/thermal_icon.png';
+import wind from './icons/wind_icon.png'
+
+export const icons = {
+    humidity,
+    thermal,
+    wind
+};
